af27ea8f06aa8edfbec857ae57089886efc00452
Farrux4401 <leganesalaves@gmail.com>
2024-02-16 11:11:26 +0500

    all commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: false,
+  loop: true,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1.7,
+      spaceBetween: 20,
+      centeredSlides: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    },
+  },
+});
+
+const systemSwiper = new Swiper(".systemSwiper", {
+  slidesPerView: 1.2,
+  spaceBetween: 30,
+  loop: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// const showMenu = document.querySelector(".show-menu");
+const navbarToggler = document.getElementById("navbar-toggler");
+const overlay = document.getElementById("overlay");
+const showBurger = document.querySelector(".show-burger");
+const closeBurger = document.querySelector(".close-burger");
+
+navbarToggler.addEventListener("click", () => {
+  overlay.classList.toggle("overlay");
+  if (navbarToggler.classList.contains("show-menu")) {
+    navbarToggler.classList.remove("show-menu");
+    showBurger.style.display = "none";
+    closeBurger.style.display = "block";
+  } else {
+    navbarToggler.classList.add("show-menu");
+    showBurger.style.display = "block";
+    closeBurger.style.display = "none";
+  }
+});
